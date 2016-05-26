d1411bf02726ae495ccd030d3d94e41d7b0b5974
diamondhead90 <diamondhead90@users.noreply.github.com>
2016-05-26 15:10:11 +0700

    Update iptables_log.py

--- iptables_log.py
+++ iptables_log.py
@@ -3,10 +3,10 @@ import re
 
 #import subprocess
 HEADER = ['IN_INTERFACE','OUT_INTERFACE','SOURCE_IP', 'SOURCE_PORT','DEST_IP','DEST_PORT','PROTOCOL']
-resultFile = open("output.csv",'wb')
+resultFile = open("log_iptables.csv",'wb')
 wr = csv.writer(resultFile, dialect='excel')
 wr.writerow(HEADER)
-filename = '/var/log/iptables.log_1'
+filename = '/var/log/iptables.log'
 RESULT = []
 #print text
 with open(filename, 'rb') as log_ip:
